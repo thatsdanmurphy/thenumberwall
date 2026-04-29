d363ae5f4b288a6af64b73cc5645213657eab7b0
Dan Murphy <dan@thenumberwall.com>
2026-04-29 12:50:20 -0400

    chore: split vendor chunk, raise chunk size limit

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,14 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1400,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+        },
+      },
+    },
+  },
 })
